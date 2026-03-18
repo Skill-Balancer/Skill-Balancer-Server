0ceeb477fc382a80320a959e805cc1d7f6b321c0
PeterDunlop <Petersimondunlop1@gmail.com>
2026-03-18 11:19:05 +0100

    added basic setup based on burn-rl-dda setup

--- src/models/environment.rs
+++ src/models/environment.rs
@@ -3,9 +3,25 @@ use burn_rl::base::{ElemType, environment::Environment};
 use crate::models::{action::GameAction, state::GameState};
 
 #[derive(Debug)]
+pub struct GameValues {}
+
+
+#[derive(Debug)]
 pub struct GameEnv {
     state: GameState,
+    reward: f32
 }
+
+
+
+impl GameEnv {
+    // The intention of this function is to set this data to the data that comes from the HTTP request
+    // from the client. I.E we need to use this function in main.
+    pub fn setDataFromClient(&mut self, result: GameValues)
+        self.state = GameState::from(result);
+        self.state = GameState::from(reward);
+}
+
 
 impl Environment for GameEnv {
     type StateType = GameState;
@@ -13,7 +29,7 @@ impl Environment for GameEnv {
     type RewardType = ElemType;
 
     fn new() -> Self {
-        todo!()
+        self.state = GameEnv::new();
     }
 
     fn state(&self) -> Self::StateType {
@@ -21,10 +37,16 @@ impl Environment for GameEnv {
     }
 
     fn reset(&mut self) -> burn_rl::base::Snapshot<Self> {
-        todo!()
+        self.state = GameState::new(); 
+        Snapshot::new(self.state, 0, false);
     }
 
+
+    // All this does is send a snapshot of the current state with the reward and set the
+    // DONE boolean to false
+    // We are keeping action because it's a part of the function signature so it might add some errors
+    // if we remove it
     fn step(&mut self, action: Self::ActionType) -> burn_rl::base::Snapshot<Self> {
-        todo!()
+        Snapshot::new(self.state, self.reward, false);
     }
 }
