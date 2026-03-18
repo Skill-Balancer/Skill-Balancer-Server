fe57549bc5cddc3652306f02ae2f4e03f0d94ef2
WannabeMo <alhemeri02@gmail.com>
2026-03-18 10:19:08 +0100

    implemented actions for our game

--- src/models/action.rs
+++ src/models/action.rs
@@ -1,22 +1,47 @@
 use burn_rl::base::Action;
 
 #[derive(Debug, Copy, Clone)]
-pub struct GameAction {}
+pub enum GameAction {
+    NoChange,
+    PlayerOneAttInc,
+    PlayerOneAttDec,
+    PlayerTwoAttInc,
+    PlayerTwoAttDec,
+}
 
 impl From<u32> for GameAction {
     fn from(value: u32) -> Self {
-        todo!()
+        match value {
+            0 => Self::NoChange,
+            1 => Self::PlayerOneAttInc,
+            2 => Self::PlayerOneAttDec,
+            3 => Self::PlayerTwoAttInc,
+            4 => Self::PlayerTwoAttDec,
+            _ => panic!("This action does not exist!"),
+        }
     }
 }
 
 impl From<GameAction> for u32 {
     fn from(action: GameAction) -> Self {
-        todo!()
+        match action {
+            GameAction::NoChange => 0,
+            GameAction::PlayerOneAttInc => 1,
+            GameAction::PlayerOneAttDec => 2,
+            GameAction::PlayerTwoAttInc => 3,
+            GameAction::PlayerTwoAttDec => 4,
+        }
     }
 }
 
 impl Action for GameAction {
     fn enumerate() -> Vec<Self> {
-        todo!()
+        vec![
+            Self::NoChange,
+            Self::PlayerOneAttInc,
+            Self::PlayerOneAttDec,
+            Self::PlayerTwoAttDec,
+            Self::PlayerTwoAttInc,
+        ]
     }
 }
