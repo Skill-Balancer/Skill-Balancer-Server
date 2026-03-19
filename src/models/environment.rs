364945fae0d8970287075c0415022b12923f95de
Peter Simon Wallace Dunlop <90600834+PeterDunlop@users.noreply.github.com>
2026-03-19 11:23:23 +0100

    Env setup (#20)
    * added basic setup based on burn-rl-dda setup
    
    * Fixed errors and added some basic new constructors for state also made reward elemtype since they are same
    
    * Fixed formatting
    
    * Update src/models/environment.rs
    
    Co-authored-by: Alfred <47451379+Alfenstein8@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Alfred <47451379+Alfenstein8@users.noreply.github.com>

--- src/models/environment.rs
+++ src/models/environment.rs
@@ -1,10 +1,26 @@
-use burn_rl::base::{ElemType, environment::Environment};
+use burn_rl::base::{ElemType, Snapshot, environment::Environment};
 
 use crate::models::{action::GameAction, state::GameState};
 
 #[derive(Debug)]
+pub struct GameValues {
+    pub data: [ElemType; 4],
+    pub reward: ElemType,
+}
+
+#[derive(Debug)]
 pub struct GameEnv {
     state: GameState,
+    reward: ElemType,
+}
+
+impl GameEnv {
+    // Set the data from HTTP from the client.
+    // This isn't used yet in main
+    pub fn set_data_from_client(&mut self, input: GameValues) {
+        self.state = GameState::from(input.data);
+        self.reward = input.reward;
+    }
 }
 
 impl Environment for GameEnv {
@@ -13,18 +29,27 @@ impl Environment for GameEnv {
     type RewardType = ElemType;
 
     fn new() -> Self {
-        todo!()
+        Self {
+            state: GameState::new(),
+            reward: 0.0,
+        }
     }
 
     fn state(&self) -> Self::StateType {
         self.state
     }
 
-    fn reset(&mut self) -> burn_rl::base::Snapshot<Self> {
-        todo!()
+    fn reset(&mut self) -> Snapshot<Self> {
+        self.state = GameState::new();
+        self.reward = 0.0;
+        Snapshot::new(self.state.clone(), self.reward, false)
     }
 
+    // All this does is send a snapshot of the current state with the reward and set the
+    // DONE boolean to false
+    // We are keeping action because it's a part of the function signature so it might add some errors
+    // if we remove it
     fn step(&mut self, action: Self::ActionType) -> burn_rl::base::Snapshot<Self> {
-        todo!()
+        Snapshot::new(self.state, self.reward, false)
     }
 }
--- src/models/state.rs
+++ src/models/state.rs
@@ -6,9 +6,26 @@ const STATE_SIZE: usize = 4; // 4 should be a variable
 
 type StateData = [ElemType; STATE_SIZE];
 
+// data has to be public so environment can access it
 #[derive(Debug, Copy, Clone)]
 pub struct GameState {
-    data: StateData,
+    pub(crate) data: StateData,
+}
+
+impl From<StateData> for GameState {
+    fn from(data: StateData) -> Self {
+        Self { data }
+    }
+}
+
+// Just used for implementing new GameStates. At a later point we should
+// Figure out what initial values are good for implementing the gamestate with.
+impl GameState {
+    pub fn new() -> Self {
+        Self {
+            data: [0.5; STATE_SIZE],
+        }
+    }
 }
 
 impl State for GameState {
