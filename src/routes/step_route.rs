a3ee36e2da70e4127f88cc5aa2d740023852bf22
WannabeMO <alhemeri02@gmail.com>
2026-03-30 10:57:07 +0200

    fix name of payload.name

--- src/routes/step_route.rs
+++ src/routes/step_route.rs
@@ -37,7 +37,7 @@ async fn create_transition(
             return (
                 StatusCode::NOT_FOUND,
                 Json(json!({
-                    "error": format!("did not find profile with id {}", payload.id)
+                    "error": format!("did not find profile with id {}", payload.name)
                 })),
             );
         }
