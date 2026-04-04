e710fe5ca12b7640d8776267bbf8d8f076c92d4b
WannabeMO <alhemeri02@gmail.com>
2026-04-04 10:08:02 +0200

    change from ndarray to wgpu, which for some reason works now

--- src/network/profile.rs
+++ src/network/profile.rs
@@ -1,8 +1,8 @@
 use crate::models::ppo::PPOTrainer;
-use burn::backend::{Autodiff, NdArray};
+use burn::backend::{Autodiff, Wgpu};
 use burn_rl::base::ElemType;
 
-type Backend = Autodiff<NdArray<ElemType>>;
+type Backend = Autodiff<Wgpu<ElemType>>;
 
 pub struct Profile {
     pub name: String,
