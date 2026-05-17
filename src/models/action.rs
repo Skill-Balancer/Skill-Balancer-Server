a90fca81c9442df3c106da780dc7e318b9e1b27f
PeterDunlop <petersimondunlop1@gmail.com>
2026-05-17 23:11:19 +0200

    changed action index given to server

--- src/models/action.rs
+++ src/models/action.rs
@@ -1,4 +1,4 @@
-use burn::tensor::TensorData;
+﻿use burn::tensor::TensorData;
 use burn::tensor::backend::Backend;
 use burn::{Tensor, tensor::Int};
 use burn_rl::base::Action;
@@ -8,19 +8,32 @@ type ActionType = i32;
 #[derive(Debug, Clone)]
 pub struct GameAction {
     pub actions: Vec<ActionType>,
+    index: ActionType,
 }
 
 impl GameAction {
     fn new(amount: usize) -> GameAction {
         GameAction {
             actions: vec![0; amount],
+            index: 0,
         }
+    }
+
+    fn categorical_index(actions: &[ActionType]) -> ActionType {
+        for (position, value) in actions.iter().enumerate() {
+            match *value {
+                1 => return position as ActionType * 2 + 1,
+                -1 => return position as ActionType * 2 + 2,
+                _ => {}
+            }
+        }
+        0
     }
 }
 
 impl Action for GameAction {
     fn to_tensor<B: Backend>(&self) -> Tensor<B, 1, Int> {
-        let tensor_data = TensorData::new(self.actions.clone(), [self.actions.len()]);
+        let tensor_data = TensorData::new(vec![self.index], [1]);
         Tensor::<B, 1, Int>::from_ints(tensor_data, &Default::default())
     }
 }
@@ -34,6 +47,7 @@ impl From<GameAction> for Vec<ActionType> {
 impl From<Vec<ActionType>> for GameAction {
     fn from(value: Vec<ActionType>) -> Self {
         let mut game_action = GameAction::new(value.len());
+        game_action.index = GameAction::categorical_index(&value);
         game_action.actions.copy_from_slice(&value);
         game_action
     }
